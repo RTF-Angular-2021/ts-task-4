74ba30deee37bfe7516d79ea30b8df7ed885c6b3
vnovikov <v.novikov@artsofte.ru>
2021-04-19 19:48:22 +0500

    Добавлены задачи для четвертого блока практик по TypeScript

---
+++ src/task_4/index.ts
@@ -0,0 +1,6 @@
+/** Задача 4
+ * Реализовать POST запрос для сохранения файла в виде blob объекта в хранилище GitHub
+ * Использовать: https://docs.github.com/en/rest/reference/git#create-a-blob
+ * Не забывайте, что пригодится аутентификация и ключ:
+ * https://docs.github.com/en/developers/apps/authenticating-with-github-apps#authenticating-as-an-installation
+*/
---
+++ src/task_5/index.ts
@@ -0,0 +1,6 @@
+/** Задача 5
+ * Реализовать GET запрос для получения blob объекта по sha ключу
+ * Использовать: https://docs.github.com/en/rest/reference/git#get-a-blob
+ * Не забывайте, что пригодится аутентификация и ключ:
+ * https://docs.github.com/en/developers/apps/authenticating-with-github-apps#authenticating-as-an-installation
+*/
